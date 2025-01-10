ec651092bee7c846a61f9baa114b11b392415e35
coolfiree <vishnu7353@gmail.com>
2025-01-10 19:41:20 +0530

    Normalize line endings

---
+++ register_form.php
@@ -0,0 +1,7 @@
+
+<form action="register_submit" action="post"\>
+    Name: <input type="text" name="name"\>
+    Email: <input type="email" name="email"\>
+    Password: <input type="password" name="password"\>
+    <input type="submit"\>
+</form>
