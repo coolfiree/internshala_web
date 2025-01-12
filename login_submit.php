67ba5ad2b1cac396c779287d4f96fb3bdfd584b2
coolfiree <vishnu7353@gmail.com>
2025-01-12 12:32:16 +0530

    my code for the login_submit.php form

--- login_submit.php
+++ login_submit.php
@@ -1,7 +1,28 @@
 <?php
+    $db_hostname = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "pglife";
+
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+
+    if (!$conn) {
+        echo("Connection failed: " . mysqli_connect_error());
+        exit;
+    }
+
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    echo $email . "<br/>";
-    echo $password . "<br/>";
+    $sql = "SELECT full_name FROM users WHERE email = '$email' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        echo "Welcome " . $row['full_name'];
+    } else {
+        echo "Invalid email or password";
+    }
+    mysqli_close($conn);
+
 ?>
